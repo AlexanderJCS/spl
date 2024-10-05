67c8cee5ac355bc21a9fee77a0e861c40fcfcb8d
Alex <alexandercastronovo@gmail.com>
2024-10-04 22:24:13 -0400

    test refactor

--- google_tests/test_tokenizer.cpp
+++ google_tests/test_tokenizer.cpp
@@ -5,53 +5,54 @@
 #include "../interpreter/tokenizer.h"
 
 TEST(TokenizerTest, AlwaysTrue) {
-    // INVALID,
-    // IDENTIFIER,
-    // OPEN_PAREN,
-    // CLOSE_PAREN,
-    // OPEN_BRACE,
-    // CLOSE_BRACE,
-    // LITERAL_INT,
-    // SEMICOLON,
-    // OPERATOR_DEFINE,
-    // OPERATOR_ADD,
-    // OPERATOR_SUB,
-    // OPERATOR_MUL,
-    // OPERATOR_DIV
+    /*
+     * All token types as of writing this test:
+     * INVALID,
+     * IDENTIFIER,
+     * OPEN_PAREN,
+     * CLOSE_PAREN,
+     * OPEN_BRACE,
+     * CLOSE_BRACE,
+     * LITERAL_INT,
+     * SEMICOLON,
+     * OPERATOR_DEFINE,
+     * OPERATOR_ADD,
+     * OPERATOR_SUB,
+     * OPERATOR_MUL,
+     * OPERATOR_DIV
+     */
 
     std::string input = "x ( { } ) 42 ; = \n \n   \t  + - * /";
-
-    token::Tokenizer tokenizer = token::Tokenizer(input);
+    token::Tokenizer tokenizer(input);
     std::vector<token::Token> tokens = tokenizer.getTokens();
 
     ASSERT_EQ(tokens.size(), 12);
 
-    ASSERT_EQ(tokens[0].type(), token::TokenType::IDENTIFIER);
-    ASSERT_EQ(tokens[0].value(), "x");
+    // Create expected token types and values
+    std::vector<std::pair<token::TokenType, std::string>> expectedTokens = {
+            {token::TokenType::IDENTIFIER, "x"},
+            {token::TokenType::OPEN_PAREN, ""},
+            {token::TokenType::OPEN_BRACE, ""},
+            {token::TokenType::CLOSE_BRACE, ""},
+            {token::TokenType::CLOSE_PAREN, ""},
+            {token::TokenType::LITERAL_INT, "42"},
+            {token::TokenType::SEMICOLON, ""},
+            {token::TokenType::OPERATOR_DEFINE, ""},
+            {token::TokenType::OPERATOR_ADD, ""},
+            {token::TokenType::OPERATOR_SUB, ""},
+            {token::TokenType::OPERATOR_MUL, ""},
+            {token::TokenType::OPERATOR_DIV, ""}
+    };
 
-    ASSERT_EQ(tokens[1].type(), token::TokenType::OPEN_PAREN);
-
-    ASSERT_EQ(tokens[2].type(), token::TokenType::OPEN_BRACE);
-
-    ASSERT_EQ(tokens[3].type(), token::TokenType::CLOSE_BRACE);
-
-    ASSERT_EQ(tokens[4].type(), token::TokenType::CLOSE_PAREN);
-
-    ASSERT_EQ(tokens[5].type(), token::TokenType::LITERAL_INT);
-    ASSERT_EQ(tokens[5].value(), "42");
-
-    ASSERT_EQ(tokens[6].type(), token::TokenType::SEMICOLON);
-
-    ASSERT_EQ(tokens[7].type(), token::TokenType::OPERATOR_DEFINE);
-
-    ASSERT_EQ(tokens[8].type(), token::TokenType::OPERATOR_ADD);
-
-    ASSERT_EQ(tokens[9].type(), token::TokenType::OPERATOR_SUB);
-
-    ASSERT_EQ(tokens[10].type(), token::TokenType::OPERATOR_MUL);
-
-    ASSERT_EQ(tokens[11].type(), token::TokenType::OPERATOR_DIV);
+    // Loop through the expected tokens and assert their properties
+    for (size_t i = 0; i < expectedTokens.size(); ++i) {
+        ASSERT_EQ(tokens[i].type(), expectedTokens[i].first);
+        if (!expectedTokens[i].second.empty()) {
+            ASSERT_EQ(tokens[i].value(), expectedTokens[i].second);
+        }
+    }
 }
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
