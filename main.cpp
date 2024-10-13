0289646bbc61873afd474580764505ee09dd2982
Alex <alexandercastronovo@gmail.com>
2024-10-12 22:30:12 -0400

    change to a program that doesn't work

--- main.cpp
+++ main.cpp
@@ -6,7 +6,13 @@ int main() {
     // todo: function calls need a comma at the end of the argument list so the ) token doesn't go into the RPN
     //  parser. This is currently intended behavior but instead should be changed to allow for no comma at the end of
     //  the argument list.
-    std::string input = "fun x(a, b) {c=b;} x(5,2,);";
+    std::string input = "fun x(a, b) {\n"
+                        "    c = b;\n"
+                        "    d = a;\n"
+                        "    return a;\n"
+                        "}\n"
+                        "\n"
+                        "x(3, 2,);";
     env::Environment env = run(input);
 
     return 0;
